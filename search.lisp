a8709b17d61743b2c42f54c77432f5a5eaa6bee2
Silas Vedder <singingfox123@protonmail.com>
2022-05-05 15:25:27 +0200

    Korrekturen.

--- search.lisp
+++ search.lisp
@@ -3,10 +3,10 @@
 (in-package #:cl-bible.search)
 
 (defun find-in-bible (bible phrase)
-  (remove-if-not (lambda (verse) (search phrase (text verse))) bible))
+  (remove-if-not (lambda (verse) (search phrase (v:text verse))) bible))
 
 (defun find-book (bible book)
-  (remove-if-not (lambda (verse) (search book (bname verse))) bible))
+  (remove-if-not (lambda (verse) (search book (v:bname verse))) bible))
 
 (defun find-chapter (book chapter)
-  (remove-if-not (lambda (verse) (string= chapter (chapter verse))) book))
+  (remove-if-not (lambda (verse) (string= chapter (v:chapter verse))) book))
